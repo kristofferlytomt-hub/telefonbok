503e8668a351942032d0259d7f5c181461b67d77
“kristofferlytomt-hub” <kristoffer.lytomt@icloud.com>
2026-09-04 15:10:27 +0200

    init

---
+++ telefonbok.py
@@ -0,0 +1,12 @@
+telefonbok = []
+person1 = {
+    "navn" : "gabriel",
+    "nummer" : "41311273"
+}
+person2 = {
+    "navn" : "sebastian",
+    "nummer" : "47955824"
+}
+telefonbok.append("person1")
+telefonbok.append("person2")
+
