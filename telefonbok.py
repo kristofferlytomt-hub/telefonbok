47bca965a0e0a3e46c4b179afa7bbf16cdee48fa
“kristofferlytomt-hub” <kristoffer.lytomt@icloud.com>
2026-09-04 15:20:34 +0200

    viser alle telf nummere

--- telefonbok.py
+++ telefonbok.py
@@ -7,6 +7,10 @@ person2 = {
     "navn" : "sebastian",
     "nummer" : "47955824"
 }
-telefonbok.append("person1")
-telefonbok.append("person2")
+telefonbok.append(person1)
+telefonbok.append(person2)
 
+def vis_alle():
+    for human_being in telefonbok:
+        print(human_being["navn"], ": ", human_being["nummer"])
+vis_alle()
